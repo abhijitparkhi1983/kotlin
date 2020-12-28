b72aacfcd18db0f63a84a506af9fd444f46ee842
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2020-12-28 15:11:50 +0300

    ++ test

--- compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/SpecialCallsWithLambdas.kt
+++ compiler/fir/analysis-tests/testData/resolveWithStdlib/problems/SpecialCallsWithLambdas.kt
@@ -1,15 +1,4 @@
-import kotlin.experimental.ExperimentalTypeInference
-
-interface FlowCollector<in T> {}
-
-@Suppress("EXPERIMENTAL_API_USAGE_ERROR")
-fun <L> flow(@BuilderInference block: suspend FlowCollector<L>.() -> Unit) = Flow(block)
-
-class Flow<out R>(private val block: suspend FlowCollector<R>.() -> Unit)
-
-fun poll72(): Flow<String> {
-    return flow {
-        val inv = {{}}!!
-        inv()
-    }
+fun foo() {
+    val inv = {{}}!!
+    val bar = {{}}
 }
